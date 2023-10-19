c4588de5c7a19563a29a4e096bc4038eb3476c96
Luan <luanpck@hotmail.com>
2023-10-19 00:51:00 -0300

    test: Implementando testes na classe dominio do catalogo

--- src/LP10.Catalogo.Domain/Produto.cs
+++ src/LP10.Catalogo.Domain/Produto.cs
@@ -14,9 +14,10 @@ namespace LP10.Catalogo.Domain
         public int QuantidadeEstoque { get; private set; }
 
         public Categoria? Categoria { get; private set; }
+        public Dimensoes Dimensoes { get; private set; }
 
         public Produto(string nome, string descricao, bool ativo, decimal valor,
-                        Guid categoriaId, DateTime dataCadastro, string imagem)
+                        Guid categoriaId, DateTime dataCadastro, string imagem, Dimensoes dimensoes)
         {
             Nome = nome;
             Descricao = descricao;
@@ -25,6 +26,9 @@ namespace LP10.Catalogo.Domain
             CategoriaId = categoriaId;
             DataCadastro = dataCadastro;
             Imagem = imagem;
+            Dimensoes = dimensoes;
+
+            Validar();
         }
 
         public void Ativar() => Ativo = true;
@@ -56,6 +60,15 @@ namespace LP10.Catalogo.Domain
         public bool PossuiEstoque(int quantidade)
         {
             return QuantidadeEstoque >= quantidade;
+        }
+
+        public void Validar()
+        {
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome do produto não pode estar vazio");
+            Validacoes.ValidarSeVazio(Descricao, "O campo Descricao do produto não pode estar vazio");
+            Validacoes.ValidarSeIgual(CategoriaId, Guid.Empty, "O campo CategoriaId do produto não pode estar vazio");
+            Validacoes.ValidarSeMenorQue(Valor, 1, "O campo Valor do produto não pode se menor igual a 0");
+            Validacoes.ValidarSeVazio(Imagem, "O campo Imagem do produto não pode estar vazio");
         }
     }
 }
